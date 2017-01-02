390593a10a5463b2470f2c95e77d2e6b9bde562f
DankBudd <evilsword54@yahoo.com>
2017-01-02 05:55:42 -0500

    Add files via upload

---
+++ game/dota_addons/our_mod_name/scripts/vscripts/addon_game_mode.lua
@@ -0,0 +1,86 @@
+-- This is the entry-point to your game mode and should be used primarily to precache models/particles/sounds/etc
+
+require('internal/util')
+require("barebones")
+require('gamemode')
+require('storageapi/json')
+require('storageapi/storage')
+
+-- This library allow for easily delayed/timed actions
+require('libraries/timers')
+-- This library can be used for advancted physics/motion/collision of units.  See PhysicsReadme.txt for more information.
+require('libraries/physics')
+-- This library can be used for advanced 3D projectile systems.
+require('libraries/projectiles')
+-- This library can be used for sending panorama notifications to the UIs of players/teams/everyone
+require('libraries/notifications')
+-- This library can be used for starting customized animations on units from lua
+require('libraries/animations')
+-- This library can be used for performing "Frankenstein" attachments on units
+require('libraries/attachments')
+-- This library can be used to synchronize client-server data via player/client-specific nettables
+require('libraries/playertables')
+-- This library can be used to create container inventories or container shops
+require('libraries/containers')
+-- This library provides a searchable, automatically updating lua API in the tools-mode via "modmaker_api" console command
+require('libraries/modmaker')
+-- This library provides an automatic graph construction of path_corner entities within the map
+require('libraries/pathgraph')
+-- This library (by Noya) provides player selection inspection and management from server lua
+require('libraries/selection')
+
+-- These internal libraries set up barebones's events and processes.  Feel free to inspect them/change them if you need to.
+require('internal/gamemode')
+require('internal/events')
+
+-- settings.lua is where you can specify many different properties for your game mode and is one of the core barebones files.
+require('settings')
+-- events.lua is where you can specify the actions to be taken when any event occurs and is one of the core barebones files.
+require('events')
+
+function Precache( context )
+--[[
+  This function is used to precache resources/units/items/abilities that will be needed
+  for sure in your game and that will not be precached by hero selection.  When a hero
+  is selected from the hero selection screen, the game will precache that hero's assets,
+  any equipped cosmetics, and perform the data-driven precaching defined in that hero's
+  precache{} block, as well as the precache{} block for any equipped abilities.
+
+  See GameMode:PostLoadPrecache() in gamemode.lua for more information
+  ]]
+
+  DebugPrint("[BAREBONES] Performing pre-load precache")
+
+  -- Particles can be precached individually or by folder
+  -- It it likely that precaching a single particle system will precache all of its children, but this may not be guaranteed
+  PrecacheResource("particle", "particles/econ/generic/generic_aoe_explosion_sphere_1/generic_aoe_explosion_sphere_1.vpcf", context)
+  PrecacheResource("particle_folder", "particles/test_particle", context)
+
+  -- Models can also be precached by folder or individually
+  -- PrecacheModel should generally used over PrecacheResource for individual models
+  PrecacheResource("model_folder", "particles/heroes/antimage", context)
+  PrecacheResource("model", "particles/heroes/viper/viper.vmdl", context)
+  PrecacheModel("models/heroes/viper/viper.vmdl", context)
+  --PrecacheModel("models/props_gameplay/treasure_chest001.vmdl", context)
+  --PrecacheModel("models/props_debris/merchant_debris_chest001.vmdl", context)
+  --PrecacheModel("models/props_debris/merchant_debris_chest002.vmdl", context)
+
+  -- Sounds can precached here like anything else
+  PrecacheResource("soundfile", "soundevents/game_sounds_heroes/game_sounds_gyrocopter.vsndevts", context)
+
+  -- Entire items can be precached by name
+  -- Abilities can also be precached in this way despite the name
+  PrecacheItemByNameSync("example_ability", context)
+  PrecacheItemByNameSync("item_example_item", context)
+
+  -- Entire heroes (sound effects/voice/models/particles) can be precached with PrecacheUnitByNameSync
+  -- Custom units from npc_units_custom.txt can also have all of their abilities and precache{} blocks precached in this way
+  PrecacheUnitByNameSync("npc_dota_hero_ancient_apparition", context)
+  PrecacheUnitByNameSync("npc_dota_hero_enigma", context)
+end
+
+-- Create the game mode when we activate
+function Activate()
+  GameRules.GameMode = GameMode()
+  GameRules.GameMode:_InitGameMode()
+end
